1ce2ee8972838b4501069a544fe2c62f194487b5
Bruno A <bralmu@gmail.com>
2014-10-15 19:27:41 +0200

    can be constructed in 3 fixed shapes

--- copistas-core/src/com/madgeargames/copista/widgets/EspectroPlayer.java
+++ copistas-core/src/com/madgeargames/copista/widgets/EspectroPlayer.java
@@ -14,8 +14,12 @@ import com.madgeargames.copista.sequences.Sequence;
 public class EspectroPlayer extends Actor {
 
 	Sprite zonas;
+	Size size;
+	Position position;
 
-	public EspectroPlayer() {
+	public EspectroPlayer(Size size, Position position) {
+		this.size = size;
+		this.position = position;
 		generarZonas(new int[] {}, null);
 	}
 
@@ -30,8 +34,14 @@ public class EspectroPlayer extends Actor {
 	}
 
 	private void generarZonas(int[] notas, NoteSet noteSet) {
-		int height = 210;
+		int height;
 		int width;
+		if (size == Size.full) {
+			height = 360;
+		} else {
+			height = 180;
+		}
+
 		Pixmap pixmap = new Pixmap(640, 360, Pixmap.Format.RGBA8888);
 		if (noteSet != null && noteSet.size() > 0) {
 			width = 640 / noteSet.size();
@@ -45,7 +55,9 @@ public class EspectroPlayer extends Actor {
 		Texture texture = new Texture(pixmap);
 		pixmap.dispose();
 		zonas = new Sprite(texture);
-		zonas.setY(-360 + height);
+		if (size == Size.half && position == Position.lower) {
+			zonas.setY(-height);
+		}
 	}
 
 	public void showNotes(Sequence sequence, float sustainTime, float silenceTime, NoteSet noteSet) {
@@ -81,5 +93,13 @@ public class EspectroPlayer extends Actor {
 		float randomVolume = (float) (0.1 + Math.random() / 10);
 		float randomPan = (float) (-.5 + Math.random());
 		sound.play(randomVolume);
+	}
+
+	public enum Size {
+		half, full;
+	}
+
+	public enum Position {
+		upper, lower;
 	}
 }
